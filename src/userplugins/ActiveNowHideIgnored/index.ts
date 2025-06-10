6f2aa59b84f992bfe7f2bc5031cd29fb999b84c4
kyrillk <kyrill.kriek@gmail.com>
2025-06-11 01:40:26 +0200

    maybe working fixed hideActiveNow partly

--- src/userplugins/ActiveNowHideIgnored/index.ts
+++ src/userplugins/ActiveNowHideIgnored/index.ts
@@ -14,24 +14,31 @@ import { contextMenus, isGuildBlacklisted, isUserBlacklisted, ResetButton } from
 export const settings = definePluginSettings({
     hideActiveNow: {
         type: OptionType.BOOLEAN,
-        description: "How to handle hidden users/ignored users in voice channel in the main Active Now section",
+        description: "Hide/Show servers instead of just users in the Active Now section",
+        default: false,
         restartNeeded: true
     },
     whitelistUsers: {
         description: "Turn the blacklist into a whitelist for users, so only the users in the list will be shown",
         type: OptionType.BOOLEAN,
-        restartNeeded: false,
+        restartNeeded: true,
     },
     whitelistServers: {
         description: "Turn the blacklist into a whitelist for server, so only the servers in the list will be shown",
         type: OptionType.BOOLEAN,
-        restartNeeded: false,
+        restartNeeded: true,
     },
     hideIgnoredUsers: {
         description: "Hide ignored users in the main Active Now section",
         type: OptionType.BOOLEAN,
         default: true,
         restartNeeded: false,
+    },
+    hideFriendsList: {
+        description: "Hide ignored users in the friends list",
+        type: OptionType.BOOLEAN,
+        default: true,
+        restartNeeded: true,
     },
     resetData: {
         type: OptionType.COMPONENT,
@@ -40,10 +47,7 @@ export const settings = definePluginSettings({
     },
 });
 
-
-
-
-// it break them yeey
+// Defines everything that the plugin does, including patches, settings, and context menus.
 export default definePlugin({
     name: "Active Now Hide Ignored",
     description: "Hides Active Now entries for ignored users.",
@@ -54,16 +58,8 @@ export default definePlugin({
         {
             find: "NOW_PLAYING_CARD_HOVERED,",
             replacement: {
-                match: /{partiedMembers:(\i)(.*)voiceChannels:(\i)(.*),\i=\i\(\)\(\i,\i\);/,
-                replace: "$&if($self.anyIgnoredUser($1) || $self.filterIgnoredGuilds($3)){return null;}",
-            },
-            predicate: () => settings.store.hideActiveNow
-        },
-        {
-            find: "NOW_PLAYING_CARD_HOVERED,",
-            replacement: {
                 match: /(\{party:)(\i)(.*?\}=\i)(.*=\i,\i=(\i)(.*),\i=\i\(\)\(\i,\i\);)/,
-                replace: "$1unfilter_$2$3,$2=$self.partyFilterIgnoredUsers(unfilter_$2)$4if($5 == 0 || $self.filterIgnoredGuilds($2)){return null;}",
+                replace: "$1unfilter_$2$3,$2=$self.partyFilterIgnoredUsers(unfilter_$2)$4if($self.shoudBeNull(unfilter_$2, $2, $5)){return null;}",
             },
             predicate: () => !settings.store.hideActiveNow
         },
@@ -85,117 +81,123 @@ export default definePlugin({
             predicate: () => settings.store.hideFriendsList
         },
     ],
-    settings,
-    isIgnoredUser(user) {
-        const userId = user.id || user;
-        if (isUserBlacklisted(userId) || (RelationshipStore.isIgnored(userId)) && settings.store.hideIgnoredUsers) {
-            return true;
-        }
-        return false;
-    },
-    anyIgnoredUser(users) {
-        return users.some(user => this.isIgnoredUser(user));
-    },
-    // party functions
-
-    partyFilterIgnoredUsers(party) {
-        var filteredPartyMembers = party.partiedMembers.filter(user => !this.isIgnoredUser(user));
-        if (filteredPartyMembers.length === 0) {
-            return { ...party, partiedMembers: filteredPartyMembers };
-        }
-        const filteredParty = {
-            ...party,
-            partiedMembers: filteredPartyMembers,
-
-            currentActivities: party.currentActivities
-                .map(activity => this.activityFilterIgnoredUsers(activity))
-                .filter(activity => activity !== null && activity !== undefined),
-            priorityMembers: party.priorityMembers
-                .map(priorityMember => this.priorityMembersFilterIgnoredUsers(party.priorityMembers, priorityMember, filteredPartyMembers))
-                .filter(priorityMember => priorityMember !== null && priorityMember !== undefined),
-            voiceChannels: party.voiceChannels
-                .map(voiceChannel => this.voiceChannelFilterIgnoredUsers(voiceChannel))
-                .filter(voiceChannel => voiceChannel !== null && voiceChannel !== undefined),
-        };
-        return filteredParty;
-    },
-
-    // needed?
-    activityFilterIgnoredUsers(activity) {
-        var filteredActivityUser = activity.activityUser;
-        var filteredPlayingMembers = activity.playingMembers.filter(user => !this.isIgnoredUser(user));
-        if (this.isIgnoredUser(activity.activityUser)) {
-            if (filteredPlayingMembers.length === 0) {
-                return null;
-            } else if (this.isIgnoredUser(activity.activityUser)) {
-                filteredActivityUser = filteredPlayingMembers[0];
-            }
-        }
-
-        // change to Const when fixed
-        var filteredActivity = {
-            ...activity,
-            activityUser: filteredActivityUser,
-            playingMembers: filteredPlayingMembers,
-        };
-
-        // console.log("activityFilterIgnoredUsers", activity, filteredActivity);
-        if (settings.store.Debug) {
-            filteredActivity = activity;
-        }
-        return filteredActivity;
-    },
-
-
-    priorityMembersFilterIgnoredUsers(priorityMembers, priorityMember, partiedMembers) {
-        var filteredUser = priorityMember.user;
-        if (this.isIgnoredUser(filteredUser)) {
-            if (partiedMembers.length === 1) {
-                return null;
-            }
-            filteredUser = partiedMembers.find(user => !priorityMembers.some(pm => pm.user.id === user.id));
-
-            if (!filteredUser) {
-                return null;
-            }
-        }
-        const filteredPriorityMember = {
-            ...priorityMember,
-            user: filteredUser,
-        };
-        return filteredPriorityMember;
-    },
-
-    voiceChannelFilterIgnoredUsers(voiceChannel) {
-        const filteredVoiceChannel = {
-            ...voiceChannel,
-            members: voiceChannel.members.filter(user => !this.isIgnoredUser(user)),
-            voiceStates: Object.fromEntries(
-                Object.entries(voiceChannel.voiceStates).filter(([userId]) =>
-                    !this.isIgnoredUser({ id: userId })
-                )
-            )
-        };
-        return filteredVoiceChannel;
-    },
-    // guild functions
-
-
-    isIgnoredGuild(guild) {
-        if (isGuildBlacklisted(guild)) {
-            return true;
-        }
-        return false;
-    },
-    // input can be a array of channels or a party
-    filterIgnoredGuilds(input) {
-        if (!input) {
-            return false;
-        }
-        var voiceChannels = input.voiceChannels || input;
-        return voiceChannels.some(voiceChannel => this.isIgnoredGuild(voiceChannel.guild.id));
-    },
-
+    isIgnoredUser,
+    partyFilterIgnoredUsers,
+    shoudBeNull,
 });
 
+function isIgnoredUser(user) {
+    const userId = user.id || user;
+    if (isUserBlacklisted(userId) || (RelationshipStore.isIgnored(userId)) && settings.store.hideIgnoredUsers) {
+        return true;
+    }
+    return false;
+}
+function anyIgnoredUser(users) {
+    return users.some(user => isIgnoredUser(user));
+}
+// party functions
 
+function partyFilterIgnoredUsers(party) {
+    var filteredPartyMembers = party.partiedMembers.filter(user => !isIgnoredUser(user));
+
+    if (filteredPartyMembers.length === 0) return { ...party, partiedMembers: filteredPartyMembers };
+
+    if (settings.store.hideActiveNow && settings.store.whitelistServers) return party;
+
+
+    const filteredParty = {
+        ...party,
+        partiedMembers: filteredPartyMembers,
+
+        currentActivities: party.currentActivities
+            .map(activity => activityFilterIgnoredUsers(activity))
+            .filter(activity => activity !== null && activity !== undefined),
+        priorityMembers: party.priorityMembers
+            .map(priorityMember => priorityMembersFilterIgnoredUsers(party.priorityMembers, priorityMember, filteredPartyMembers))
+            .filter(priorityMember => priorityMember !== null && priorityMember !== undefined),
+        voiceChannels: party.voiceChannels
+            .map(voiceChannel => voiceChannelFilterIgnoredUsers(voiceChannel))
+            .filter(voiceChannel => voiceChannel !== null && voiceChannel !== undefined),
+    };
+    return filteredParty;
+}
+
+// needed?
+function activityFilterIgnoredUsers(activity) {
+    var filteredActivityUser = activity.activityUser;
+    var filteredPlayingMembers = activity.playingMembers.filter(user => !isIgnoredUser(user));
+    if (isIgnoredUser(activity.activityUser)) {
+        if (filteredPlayingMembers.length === 0) {
+            return null;
+        } else if (isIgnoredUser(activity.activityUser)) {
+            filteredActivityUser = filteredPlayingMembers[0];
+        }
+    }
+
+    // change to Const when fixed
+    var filteredActivity = {
+        ...activity,
+        activityUser: filteredActivityUser,
+        playingMembers: filteredPlayingMembers,
+    };
+
+    return filteredActivity;
+}
+
+function priorityMembersFilterIgnoredUsers(priorityMembers, priorityMember, partiedMembers) {
+    var filteredUser = priorityMember.user;
+    if (isIgnoredUser(filteredUser)) {
+        if (partiedMembers.length === 1) {
+            return null;
+        }
+        filteredUser = partiedMembers.find(user => !priorityMembers.some(pm => pm.user.id === user.id));
+
+        if (!filteredUser) {
+            return null;
+        }
+    }
+    const filteredPriorityMember = {
+        ...priorityMember,
+        user: filteredUser,
+    };
+    return filteredPriorityMember;
+}
+
+function voiceChannelFilterIgnoredUsers(voiceChannel) {
+    const filteredVoiceChannel = {
+        ...voiceChannel,
+        members: voiceChannel.members.filter(user => !isIgnoredUser(user)),
+        voiceStates: Object.fromEntries(
+            Object.entries(voiceChannel.voiceStates).filter(([userId]) =>
+                !isIgnoredUser({ id: userId })
+            )
+        )
+    };
+    return filteredVoiceChannel;
+}
+// guild functions
+
+
+function isIgnoredGuild(guild) {
+    if (isGuildBlacklisted(guild)) {
+        return true;
+    }
+    return false;
+}
+// input can be a array of channels or a party
+function filterIgnoredGuilds(input) {
+    if (!input) {
+        return false;
+    }
+    var voiceChannels = input.voiceChannels || input;
+    return voiceChannels.some(voiceChannel => isIgnoredGuild(voiceChannel.guild.id));
+}
+
+// add logic for whitelist
+function shoudBeNull(unFilteredParty, filteredParty, partySize) {
+    if (!unFilteredParty || !filteredParty || !partySize) return true;
+    if (partySize === 0 || filterIgnoredGuilds(unFilteredParty)) return true;
+    // if (anyIgnoredUser(unFilteredParty) && settings.store.hideActiveNow && !settings.store.whitelistServers) return true;
+    return false;
+}
